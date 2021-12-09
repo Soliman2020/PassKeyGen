c1198e0b1d6dbdc2678683bbf4c21ebee56dfaa5
Soliman2020 <35764807+Soliman2020@users.noreply.github.com>
2021-12-09 11:58:01 +0200

    Update passFounder.py
    # MegaByte did a great function indeed but while running it I got some IndexErrors and I did some editing.
    
    # It works like charm now :)

--- Program/passFounder.py
+++ Program/passFounder.py
@@ -1,36 +1,34 @@
-#-------------------------------------------------------------------------------
-# Name:        passFounder
-# Purpose:
-#
-# Author:      MegaByte
-#
-# Created:     07/12/2021
-# Copyright:   (c) MegaByte 2021
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 import random
 
 def passKeyGen():
+
+    '''
+    Name:        passFounder
+    Purpose:     Password random generator (length 16)
+    Author:      MegaByte
+    Editor:      Soliman2020
+    Created:     07/12/2021
+    Updated:     09/12/2021
+    Copyright:   (c) MegaByte 2021
+    Licence:     <your licence>
+    '''
+
     passW = ""
+
     for i in range(0, 16):
-        tmp = random.randint(0, 4)
+        tmp = random.randint(1, 4)  # using 1 instead of 0 to keep the output passW length 16.
         if tmp == 1:
-            txt = random.randint(0, len(txt_Lower))
-            passW = passW + (txt_Lower[txt])
-           #print(passW)
+            index = random.randint(0, len(txt_Lower)-1)   # length-1 to avoid IndexError: list index out of range
+            passW = passW + (txt_Lower[index])
         if tmp == 2:
-            txt = random.randint(0, len(txt_Upper))
-            passW = passW + (txt_Upper[txt])
-            print(passW)
+            index = random.randint(0, len(txt_Upper)-1)   # length-1 to avoid IndexError: list index out of range
+            passW = passW + (txt_Upper[index])
         if tmp == 3:
-            txt = random.randint(0, len(txt_Int))
-            passW = passW + (txt_Int[txt])
-            #print(passW)
+            index = random.randint(0, len(txt_Int)-1)     # length-1 to avoid IndexError: list index out of range
+            passW = passW + (txt_Int[index])
         if tmp == 4:
-            txt = random.randint(0, len(txt_Symbol))
-            passW = passW + (txt_Symbol[txt])
-            #print(passW)
+            index = random.randint(0, len(txt_Symbol)-1)  # length-1 to avoid IndexError: list index out of range
+            passW = passW + (txt_Symbol[index])
     print("Your password is",passW)
 
 
@@ -39,11 +37,6 @@ txt_Upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
 txt_Int = ["0","1","2","3","4","5","6","7","8","9"]
 txt_Symbol = ["£","$","%","^","&","*","(",")","_","-","+","=","[","]","{","}","'","@",":",";","#","~","?","|",".",","]
 tmp = 0
-txt = 0
-
+index = 0
 
 passKeyGen()
-
-
-
-
